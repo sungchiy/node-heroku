2d4bb800d3dca6ce91f03ee59516bd4d2a7fdf64
Charlie Sung <charlie.sung@accedo.tv>
2015-12-30 22:44:34 +0800

    adding mongoose

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express'),
     port = process.env.PORT || 4040,
+    mongoose = require('mongoose'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
     app = express();
 
-app.get('/', function(req,res){
-    res.send("my awesome app");
+mongoose.connect('mongodb://localhost/sungchiy');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(logger('dev'));
+app.set('view engine','ejs');
+
+app.get('/', function(req,res,next){
+    res.send("my awesome mongoose app");
 });
 
 app.listen(port, function(err){
